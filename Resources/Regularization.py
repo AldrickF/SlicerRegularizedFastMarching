850a57e5d4c12ecf60a53538bf4c20a6b601cb49
AldrickF <aldric91@hotmail.fr>
2020-12-11 17:36:08 +0100

    First commit

---
+++ Resources/Regularization.py
@@ -0,0 +1,252 @@
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#STEP 1: FUNCTIONS TO COMPUTE THE REGULARIZATION FIELD
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def Dilation_3D_array(InputArray,RSE):
+  """
+  Perform the dilation of a 3D array
+  InputArray: is a 3D array
+  RadiusStructuringElement: Radius of the structuring element -> The structuring element is a cube of size (1+2*RSE) x (1+2*RSE) x (1+2*RSE)
+  """
+
+  #1) init
+  DilatedArray_tmp=InputArray.copy()
+  DilatedArray=InputArray.copy()
+  [NX,NY,NZ]=InputArray.shape
+
+  #2) dilation on the 1st axis
+  #2.1) first slices
+  DataInSE=DilatedArray_tmp[0:2*RSE+1,:,:]
+  Max_DataInSE=DataInSE.max(axis=0)
+
+  for slice_ID in range(0,RSE+1):
+      DilatedArray[slice_ID,:,:]=Max_DataInSE[:,:]
+
+  #2.2) middle slices
+  NewSlice_in_OrigImage=2*RSE+1
+  LastSlice_in_SlidingWindow=0
+  for slice_ID in  range(RSE+1,NX-RSE):
+      #get data
+      DataInSE[LastSlice_in_SlidingWindow,:,:]=DilatedArray_tmp[NewSlice_in_OrigImage,:,:]
+
+      #compute the max
+      Max_DataInSE=DataInSE.max(axis=0)
+
+      #update DilatedArray
+      DilatedArray[slice_ID,:,:]=Max_DataInSE[:,:]
+
+      #prepare the next iteration
+      NewSlice_in_OrigImage+=1
+      LastSlice_in_SlidingWindow+=1
+      if LastSlice_in_SlidingWindow>2*RSE:
+          LastSlice_in_SlidingWindow=0
+
+  #2.3) last slices
+  for slice_ID in  range(NX-RSE,NX):
+      DilatedArray[slice_ID,:,:]=Max_DataInSE[:,:]
+
+  #3) dilation on the 2nd axis
+
+  #3.0) init
+  DilatedArray_tmp=DilatedArray.copy()
+
+  #3.1) first slices
+  DataInSE=DilatedArray_tmp[:,0:2*RSE+1,:]
+  Max_DataInSE=DataInSE.max(axis=1)
+
+  for slice_ID in range(0,RSE+1):
+      DilatedArray[:,slice_ID,:]=Max_DataInSE[:,:]
+
+  #3.2) middle slices
+  NewSlice_in_OrigImage=2*RSE+1
+  LastSlice_in_SlidingWindow=0
+  for slice_ID in  range(RSE+1,NY-RSE):
+      #get data
+      DataInSE[:,LastSlice_in_SlidingWindow,:]=DilatedArray_tmp[:,NewSlice_in_OrigImage,:]
+
+      #compute the max
+      Max_DataInSE=DataInSE.max(axis=1)
+
+      #update DilatedArray
+      DilatedArray[:,slice_ID,:]=Max_DataInSE[:,:]
+
+      #prepare the next iteration
+      NewSlice_in_OrigImage+=1
+      LastSlice_in_SlidingWindow+=1
+      if LastSlice_in_SlidingWindow>2*RSE:
+          LastSlice_in_SlidingWindow=0
+
+  #3.3) last slices
+  for slice_ID in  range(NY-RSE,NY):
+      DilatedArray[:,slice_ID,:]=Max_DataInSE[:,:]
+
+  #4) dilation on the 3rd axis
+
+  #4.0) init
+  DilatedArray_tmp=DilatedArray.copy()
+
+  #4.1) first slices
+  DataInSE=DilatedArray_tmp[:,:,0:2*RSE+1]
+  Max_DataInSE=DataInSE.max(axis=2)
+
+  for slice_ID in range(0,RSE+1):
+      DilatedArray[:,:,slice_ID]=Max_DataInSE[:,:]
+
+  #4.2) middle slices
+  NewSlice_in_OrigImage=2*RSE+1
+  LastSlice_in_SlidingWindow=0
+  for slice_ID in  range(RSE+1,NZ-RSE):
+      #get data
+      DataInSE[:,:,LastSlice_in_SlidingWindow]=DilatedArray_tmp[:,:,NewSlice_in_OrigImage]
+
+      #compute the max
+      Max_DataInSE=DataInSE.max(axis=2)
+
+      #update DilatedArray
+      DilatedArray[:,:,slice_ID]=Max_DataInSE[:,:]
+
+      #prepare the next iteration
+      NewSlice_in_OrigImage+=1
+      LastSlice_in_SlidingWindow+=1
+      if LastSlice_in_SlidingWindow>2*RSE:
+          LastSlice_in_SlidingWindow=0
+
+  #4.3) last slices
+  for slice_ID in  range(NZ-RSE,NZ):
+      DilatedArray[:,:,slice_ID]=Max_DataInSE[:,:]
+
+
+  return DilatedArray
+
+
+def Erosion_3D_array(InputArray,RSE):
+  """
+  Perform the erosion of a 3D array
+  InputArray: is a 3D array
+  RadiusStructuringElement: Radius of the structuring element -> The structuring element is a cube of size (1+2*RSE) x (1+2*RSE) x (1+2*RSE)
+  """
+
+  #1) init
+  ErodedArray_tmp=InputArray.copy()
+  ErodedArray=InputArray.copy()
+  [NX,NY,NZ]=InputArray.shape
+
+  #2) dilation on the 1st axis
+  #2.1) first slices
+  DataInSE=ErodedArray_tmp[0:2*RSE+1,:,:]
+  Min_DataInSE=DataInSE.min(axis=0)
+
+  for slice_ID in range(0,RSE+1):
+      ErodedArray[slice_ID,:,:]=Min_DataInSE[:,:]
+
+  #2.2) middle slices
+  NewSlice_in_OrigImage=2*RSE+1
+  LastSlice_in_SlidingWindow=0
+  for slice_ID in  range(RSE+1,NX-RSE):
+      #get data
+      DataInSE[LastSlice_in_SlidingWindow,:,:]=ErodedArray_tmp[NewSlice_in_OrigImage,:,:]
+
+      #compute the min
+      Min_DataInSE=DataInSE.min(axis=0)
+
+      #update ErodedArray
+      ErodedArray[slice_ID,:,:]=Min_DataInSE[:,:]
+
+      #prepare the next iteration
+      NewSlice_in_OrigImage+=1
+      LastSlice_in_SlidingWindow+=1
+      if LastSlice_in_SlidingWindow>2*RSE:
+          LastSlice_in_SlidingWindow=0
+
+  #2.3) last slices
+  for slice_ID in  range(NX-RSE,NX):
+      ErodedArray[slice_ID,:,:]=Min_DataInSE[:,:]
+
+  #3) dilation on the 2nd axis
+
+  #3.0) init
+  ErodedArray_tmp=ErodedArray.copy()
+
+  #3.1) first slices
+  DataInSE=ErodedArray_tmp[:,0:2*RSE+1,:]
+  Min_DataInSE=DataInSE.min(axis=1)
+
+  for slice_ID in range(0,RSE+1):
+      ErodedArray[:,slice_ID,:]=Min_DataInSE[:,:]
+
+  #3.2) middle slices
+  NewSlice_in_OrigImage=2*RSE+1
+  LastSlice_in_SlidingWindow=0
+  for slice_ID in  range(RSE+1,NY-RSE):
+      #get data
+      DataInSE[:,LastSlice_in_SlidingWindow,:]=ErodedArray_tmp[:,NewSlice_in_OrigImage,:]
+
+      #compute the min
+      Min_DataInSE=DataInSE.min(axis=1)
+
+      #update ErodedArray
+      ErodedArray[:,slice_ID,:]=Min_DataInSE[:,:]
+
+      #prepare the next iteration
+      NewSlice_in_OrigImage+=1
+      LastSlice_in_SlidingWindow+=1
+      if LastSlice_in_SlidingWindow>2*RSE:
+          LastSlice_in_SlidingWindow=0
+
+  #3.3) last slices
+  for slice_ID in  range(NY-RSE,NY):
+      ErodedArray[:,slice_ID,:]=Min_DataInSE[:,:]
+
+  #4) dilation on the 3rd axis
+
+  #4.0) init
+  ErodedArray_tmp=ErodedArray.copy()
+
+  #4.1) first slices
+  DataInSE=ErodedArray_tmp[:,:,0:2*RSE+1]
+  Min_DataInSE=DataInSE.min(axis=2)
+
+  for slice_ID in range(0,RSE+1):
+      ErodedArray[:,:,slice_ID]=Min_DataInSE[:,:]
+
+  #4.2) middle slices
+  NewSlice_in_OrigImage=2*RSE+1
+  LastSlice_in_SlidingWindow=0
+  for slice_ID in  range(RSE+1,NZ-RSE):
+      #get data
+      DataInSE[:,:,LastSlice_in_SlidingWindow]=ErodedArray_tmp[:,:,NewSlice_in_OrigImage]
+
+      #compute the min
+      Min_DataInSE=DataInSE.min(axis=2)
+
+      #update ErodedArray
+      ErodedArray[:,:,slice_ID]=Min_DataInSE[:,:]
+
+      #prepare the next iteration
+      NewSlice_in_OrigImage+=1
+      LastSlice_in_SlidingWindow+=1
+      if LastSlice_in_SlidingWindow>2*RSE:
+          LastSlice_in_SlidingWindow=0
+
+  #4.3) last slices
+  for slice_ID in  range(NZ-RSE,NZ):
+      ErodedArray[:,:,slice_ID]=Min_DataInSE[:,:]
+
+
+  return ErodedArray
+
+
+def regularization(InputImage, StructuringElementRadius=3):
+    """
+    Compute the 3D scalar field that will be used to regularize the seeds propagation
+    Inputs:
+      * InputImage: the 3D image that will be segmented. Must be a 3D numpy array.
+      * StructuringElementRadius: A structuring element of size (1+2*StructuringElementRadius) x (1+2*StructuringElementRadius) x (1+2*StructuringElementRadius) will be used
+    Outputs:
+      * R: The 3D numpy array having the same size as InputImage, used for the regularization
+    """
+
+    DilatedArray=Dilation_3D_array(InputImage, StructuringElementRadius)
+    ErrodedArray=Erosion_3D_array(InputImage, StructuringElementRadius)
+    
+    return DilatedArray-ErrodedArray
---
+++ Resources/Segmentation.py
@@ -0,0 +1,226 @@
+import numpy as np
+import os.path
+import time
+import csv
+
+import math
+
+from Resources.Regularization import *
+
+# import cProfile, pstats, io
+
+def getListIJKToRas(inputVolume, points):
+    """
+    """
+    for i in range(len(points)): # pour chaque point 
+        points[i] = getIJKToRas(inputVolume, points[i])
+
+    return points
+
+def getIJKToRas(inputVolume, point):
+    """
+    """
+    # Get voxel position in IJK coordinate system
+    point_Kji = point
+    point_Ijk = [point_Kji[2], point_Kji[1], point_Kji[0]]
+    
+    # Get physical coordinates from voxel coordinates
+    volumeIjkToRas = vtk.vtkMatrix4x4()
+    inputVolume.GetIJKToRASMatrix(volumeIjkToRas)
+    point_VolumeRas = [0, 0, 0, 1]
+    volumeIjkToRas.MultiplyPoint(np.append(point_Ijk,1.0), point_VolumeRas)
+    
+    # If volume node is transformed, apply that transform to get volume's RAS coordinates
+    transformVolumeRasToRas = vtk.vtkGeneralTransform()
+    slicer.vtkMRMLTransformNode.GetTransformBetweenNodes(inputVolume.GetParentTransformNode(), None, transformVolumeRasToRas)
+    return transformVolumeRasToRas.TransformPoint(point_VolumeRas[0:3])
+    
+######
+######
+
+def getMasks(img, seeds, nbLabel, marginMask):
+    """
+    Return for each label a mask built with the two extremum seeds including the margin,
+    excluding the background seeds that use a unique mask  
+    """
+    maskMin = np.ones(3, dtype=int)
+    maskMax = np.array([img.shape[0], img.shape[1], img.shape[2]]) - maskMin
+    marginMask = [marginMask] * 3
+    masksByLabel = []
+    for i in range(nbLabel-1):
+        masksByLabel.append(np.array([maskMax, maskMin]))
+    
+    # Find the extremums for each label
+    # If background seed : add mask by seed
+    for seed in seeds:
+        label = seed.get("label") - 1
+        if label >= nbLabel - 1: # Back ground seed
+            masksByLabel.append( [seed.get("pos"), seed.get("pos")] )
+        else : 
+            masksByLabel[label][0] = np.minimum(seed.get("pos"), masksByLabel[label][0])
+            masksByLabel[label][1] = np.maximum(seed.get("pos"), masksByLabel[label][1])
+        
+    # Add the margin for each label
+    for i in range(len(masksByLabel)):
+        borneMin = np.array(masksByLabel[i][0]) - np.array(marginMask)
+        borneMax = np.array(masksByLabel[i][1]) + np.array(marginMask)
+        masksByLabel[i][0] = np.maximum(borneMin, maskMin)
+        masksByLabel[i][1] = np.minimum(borneMax, maskMax)
+    
+    return masksByLabel
+
+def getDistanceBetweenPixel(Ip, Iq, gamma, R, p, q, imageSpacing):
+    """
+    Return the distance between two voxels depending on their intensities and the regularization cost
+    """
+    delta = 4.0/1.5625 if imageSpacing[2] else 1
+    
+    R = int(R)
+    
+    # if p[0] != q[0]:
+    #     delta = imageSpacing[0]
+    # elif p[1] != q[1]:
+    #     delta = imageSpacing[1]
+    # elif p[2] != q[2]:
+    #     delta = imageSpacing[2]
+        
+    return math.sqrt(delta * math.pow(Ip - Iq, 2) + delta * gamma * math.pow(R, 2))
+
+
+def isPixelInMaskArea(mask, voxel):
+    """
+    Check if this voxel is in this mask
+    """
+    # return mask[voxel[0], voxel[1], voxel[2]] == 1
+    for i in range(len(voxel)):
+        if voxel[i] < mask[0][i] or voxel[i] > mask[1][i]:
+            return False
+    return True  
+
+def clip(x, xMin, xMax):
+    """
+    Return the clipped x value between the interval [xMin, xMax]
+    """
+    return max(xMin, min(x, xMax))
+
+def segmentation(globalPath, volume, voxels, seeds, nbLabel, marginMask, distance, gamma, regDiameter, threshold):
+    """
+    Return the label s image containing the voxels linked to each seed
+    """
+    ###### Preparation des donnees 
+    
+
+    # cp = cProfile.Profile()
+    # cp.enable()
+
+
+    # Creation des masques en fonction des positions des graines et taille max du mask
+    masks = getMasks(voxels, seeds, nbLabel, marginMask)
+
+    # Image des masques
+    imgMasks = np.zeros(voxels.shape, dtype=int)
+
+    for m in masks:
+        imgMasks[m[0][0]:m[1][0], m[0][1]:m[1][1], m[0][2]:m[1][2]] = 1
+    
+    # Map de regularisation - dilatation de l'image
+    start_time = time.time()
+    R = np.copy(voxels)
+
+    # Verifie si cette regularisation a deja ete faite    
+    regularizationFile = globalPath + "Regularizations/" + volume.GetName() + "_" + str(regDiameter) + ".regu.npy"
+    if os.path.isfile(regularizationFile):
+        print("--- Alredy existing regularization")
+        R = np.load(regularizationFile)
+    else :
+        # R = np.copy(voxels)
+        print("- Regularization start")
+        R = regularization(voxels, regDiameter)
+        print("- Regularization end")
+        np.save(regularizationFile, R)
+    reguliarization_time = time.time() - start_time
+    print("- Regularization time : %s seconds -" % reguliarization_time)
+    
+    ###### Affichage de l'initialisation
+    print("Nombre de seeds : " + str(len(seeds)))
+    # print("Seeds : " + str(seeds))
+    # print("Voxels shape : " + str(voxels.shape))
+    # print("Masques :", masks)
+
+    ###### Algorithme
+    start_time = time.time()
+    
+    # Image des labels
+    imgLabel = np.ndarray(shape=voxels.shape, dtype=int)
+    imgLabel.fill(0)
+    
+    # Image des distances
+    imgDist = np.ndarray(shape=voxels.shape, dtype=float)
+    imgDist.fill(distance)
+    
+    # Image des pixels parcourus
+    imgPixelParcourus = np.zeros(voxels.shape, dtype=int)
+    
+    # Liste des pixels courant a parcourir
+    listCurrentPixels = []
+    # Liste des prochains pixels a parcourir
+    listNextPixels = []
+    
+    # Initialisation des images avec les graines
+    for l in range(len(seeds)):
+        pos = seeds[l].get("pos")
+        listNextPixels.append([pos[0], pos[1], pos[2]])
+        imgDist[pos[0], pos[1], pos[2]] = 0
+        imgLabel[pos[0], pos[1], pos[2]] = seeds[l].get("label")
+        
+    imageSpacing = volume.GetSpacing()
+
+    voisins = [np.array([-1, 0, 0]), np.array([1, 0, 0]), np.array([0, -1, 0]), np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([0, 0, 1])]
+
+    while listNextPixels != []:
+
+        listCurrentPixels = list(listNextPixels)
+        listNextPixels = []                    # Liste des prochains pixels a parcourir
+
+        imgPixelParcourus[1:-1,1:-1,1:-1] = 0    #masque compagnon de listNextPixels
+        
+        for p in listCurrentPixels:
+            voxelP = voxels[p[0], p[1], p[2]]
+            label_p = imgLabel[p[0], p[1], p[2]]
+            m = masks[label_p - 1]                
+
+            # Calcul la distance des pixels voisins
+            for v in voisins:
+                q = np.add(p, v)
+                voxelQ = voxels[q[0], q[1], q[2]]
+    
+                # imgPixelParcourus[q[0], q[1], q[2]] == 1
+                if not isPixelInMaskArea(m, [q[0], q[1], q[2]]) or imgPixelParcourus[q[0], q[1], q[2]] == 1 or voxelQ < threshold[0] or voxelQ > threshold[1]:
+                    continue
+
+                DistBetweenVoxels = getDistanceBetweenPixel(voxelP, voxelQ, gamma, R[q[0], q[1], q[2]], p, q, imageSpacing)
+                DistToSeed = imgDist[p[0], p[1], p[2]]+DistBetweenVoxels
+
+                if imgDist[q[0], q[1], q[2]] > DistToSeed:    #remark : imgDist is initialized to distance everywhere, except at the seed locations
+                    imgDist[q[0], q[1], q[2]] = DistToSeed
+                    imgLabel[q[0], q[1], q[2]] = label_p
+                    listNextPixels.append(q)
+                    # imgPixelParcourus[q[0], q[1], q[2]] = 1
+                    imgPixelParcourus[q[0], q[1], q[2]] = 1
+
+    imgLabel = np.clip(imgLabel, 0, nbLabel)
+
+    # cp.disable()
+    # #s = io.StringIO()
+    # f = open(globalPath + 'segm.prof', 'w+')
+    # sortby = 'cumulative'
+    # ps = pstats.Stats(cp, stream=f).sort_stats(sortby).print_stats()
+    # f.close()
+
+    #print(s.getvalue())
+
+    ###### Affichage des resultats
+    print("- Regularization time : %s seconds -" % reguliarization_time)
+    print("- Segmentation time :   %s seconds -" % (time.time() - start_time))
+
+    return imgLabel
